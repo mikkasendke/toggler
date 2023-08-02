ab50f1f892c71d819226e2a89ce15e0d7448610c
Mikka <mikka@sendke.eu>
2023-08-02 03:58:37 +0200

    First steps + planning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use winsafe::{prelude::*, Sleep};
+use winsafe::{co::MB, co::WS, co::WS_EX};
+use winsafe::HWND;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut last_focused: HWND = winsafe::HWND::NULL;
+    let mut subject: HWND = winsafe::HWND::NULL;
+
+
+    // Set subject on keypress
+    //
+    // Check if hotkey is pressed
+    // If yes:
+    // - Get focused window
+    // - Check if focused window is subject
+    // - If yes:
+    // -- Set focused window to last_focused window if it exists
+    // - If no:
+    // -- Set last_focused to focused window
+    // -- Set focused window to subject
+    // If no:
+    // - Do nothing
+    //
+    // Sleep for some time
+    // Repeat
+    loop {
+        let woah = match winsafe::HWND::GetForegroundWindow() {
+            Some(focused) => {
+                println!("focused: {:?}", focused);
+                focused
+            }
+            None => {
+                println!("Error: No focused window found.");
+                winsafe::HWND::NULL
+            }
+        };
+        if woah == winsafe::HWND::NULL {
+            println!("woah is null");
+        }
+        println!("woah: {:?}", woah);
+        println!("is_focused(&woah): {:?}", is_focused(&woah));
+        // assert!(is_focused(&woah));
+
+        Sleep(1);
+    }
+}
+
+fn is_focused(hwnd: &HWND) -> bool {
+    match winsafe::HWND::GetForegroundWindow() {
+        Some(focused) => {
+            return &focused == hwnd;
+        }
+        None => {
+            println!("Error: No focused window found.");
+            return false;
+        }
+    }
 }
